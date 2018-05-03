f5d75dc95fc1ed6b5055d5c369e5970ed36e342d
James20381 <pikej307@gmail.com>
2018-05-03 16:47:59 +1000

    test

---
+++ protect.js
@@ -0,0 +1,8 @@
+var password;
+var correctpassword="23!rkW#8";
+password=prompt('Please enter password to view this site:','');
+if (password==correctpassword)
+alert('Click Ok to enter');
+else {
+    window.location="https://www.google.com.au/";
+}
